dca69a5de9f57be7b4761658655490db4d2e8f5a
jamesmarc2016 <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>
2021-08-21 19:39:31 +0800

    111

--- jd_shareActivity.js
+++ jd_shareActivity.js
@@ -52,6 +52,7 @@ if(process.env.SHARE_ACTIVITY_URL){
             $.index = i + 1;
             $.isLogin = true;
             $.nickName = '';
+            console.log(`\n\n******开始获取助力数据【京东账号${$.index}】${$.nickName || $.UserName}*********\n`);
             if (!$.isLogin) {
                 $.msg($.name, `【提示】cookie已失效`, `京东账号${$.index} ${$.nickName || $.UserName}\n请重新登录获取\nhttps://bean.m.jd.com/bean/signIndex.action`, {
                     "open-url": "https://bean.m.jd.com/bean/signIndex.action"
